fda52c9a52dffbd81780200022fe6e68833b7c32
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-10-23 16:29:45 +0530

    Added: login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import AllRoutes from "./components/Routes";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world mayur!</h1>
+      <AllRoutes />
     </>
   );
 }
