3d1b9c752937591b25f768c2652c1db89c099be0
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-10-23 16:54:59 +0530

    Added: bg image and text

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <nav className="w-full h-12 absolute flex content-center px-2">
+      <h1 className="text-[2rem] w-36 font-bold text-red-800 bg-gradient-to-b from-black">
+        GPT-FLIX
+      </h1>
+    </nav>
+  );
+};
+export default Navbar;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,17 @@
+import Navbar from "../components/Navbar";
+
 const Login = () => {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/7c0e18aa-2c95-474d-802e-7f30e75dcca4/web/IN-en-20241014-TRIFECTA-perspective_e7121311-c11e-4809-a3e6-22abffa33569_small.jpg"
+          alt="image"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Login;
