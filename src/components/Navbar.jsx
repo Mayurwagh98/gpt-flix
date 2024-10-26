255329e4e176075559064349215fda2505f7617f
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-10-26 11:50:56 +0530

    Added: photo url and sign out btn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
+import { useSelector } from "react-redux";
+
 const Navbar = () => {
+  const user = useSelector((store) => store.userReducer);
   return (
-    <nav className="w-full h-12 absolute flex content-center px-2 z-10">
+    <nav className="w-full h-12 absolute flex justify-between items-center px-2 z-10">
       <h1 className="text-[3rem] w-full font-bold text-[#C62E2E] mx-32 my-2 contrast-150">
         GPTFLIX
       </h1>
+      {user && (
+        <div className="flex w-1/5 py-2">
+          <img
+            src="https://avatars.githubusercontent.com/u/69896733?v=4"
+            alt="img"
+            className="mx-4 w-9"
+          />
+          <button className="bg-black text-white p-2 text-sm rounded flex justify-center items-center">
+            Sign Out
+          </button>
+        </div>
+      )}
     </nav>
   );
 };
