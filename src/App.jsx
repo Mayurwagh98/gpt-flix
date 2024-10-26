5f58b4db807f2114c13d6353dc3a2290c5a68fd7
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-10-26 11:42:29 +0530

    Added: onAuthStateChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { useEffect } from "react";
 import "./App.css";
 import AllRoutes from "./components/Routes";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./utils/firebase";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addUser, removeUser } from "./utils/userSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          }),
+        );
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+  }, []);
   return (
     <>
       <AllRoutes />
--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
+import Browse from "../pages/Browse";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/browse" element={<Browse />} />
     </Routes>
   );
 };
