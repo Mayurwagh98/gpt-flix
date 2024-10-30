0a09e19d750213c33ed47301cd1d25421f180c94
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-10-30 15:48:23 +0530

    Added: custom hook for fetching trailer

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,35 +1,19 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
-import { addMovieTrailer } from "../utils/nowPlayingMoviesSlice";
-import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../customHooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
-  const dispatch = useDispatch();
-  const getMovieVideo = async () => {
-    const data = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
-      API_OPTIONS,
-    );
-    const json = await data.json();
+  useMovieTrailer(movieId);
 
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-
-    const singleTrailer = filterData?.length ? filterData[0] : json.results[0];
-    console.log(singleTrailer);
-
-    dispatch(addMovieTrailer(singleTrailer));
-  };
-
-  useEffect(() => {
-    getMovieVideo();
-  }, []);
+  const movieTrailer = useSelector(
+    (store) => store.nowPlayingMovies?.movieTrailer,
+  );
 
   return (
     <div>
       <iframe
         width="560"
         height="315"
-        src={`https://www.youtube.com/embed/FKBN1qAzW3s?si=WbtQr6GR57TY9lgZ`}
+        src={`https://www.youtube.com/embed/${movieTrailer?.key}?si=WbtQr6GR57TY9lgZ`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
