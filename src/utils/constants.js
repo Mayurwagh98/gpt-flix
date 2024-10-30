e2172f1817c2892c3c9aa59ab33f10dbdec568ae
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-10-30 13:30:04 +0530

    Made: nowplyaing movies api call

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,12 @@
-export const photo_url = "https://avatars.githubusercontent.com/u/69896733?v=4"
-export const user_avatar = "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e"
+export const photo_url = "https://avatars.githubusercontent.com/u/69896733?v=4";
+export const user_avatar =
+  "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+export const TMDB_API_KEY = "617f9a5ccb7eead92ca3c0b26e849018";
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MTdmOWE1Y2NiN2VlYWQ5MmNhM2MwYjI2ZTg0OTAxOCIsIm5iZiI6MTczMDI3NDk1My44NTk5NTI0LCJzdWIiOiI2NzIxZTA3MjFkMGI1NzFiOGE1YzFhNzciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.oaz6_dJb-bS0KDSWg7_wtCFpOJNbN80isxqsE6kSYiU",
+  },
+};
