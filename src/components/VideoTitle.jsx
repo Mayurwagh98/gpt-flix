b4c6ee72fa3867f4152b278a7d28d28b59ab14d7
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-10-30 15:56:04 +0530

    Added: icons in buttons

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -1,14 +1,17 @@
+import { FaPlay } from "react-icons/fa";
+import { IoMdInformationCircleOutline } from "react-icons/io";
+
 const VideoTitle = ({ movieTitle, movieDesc }) => {
   return (
     <div className="py-[10rem] px-20 ">
       <h1 className="text-4xl font-bold">{movieTitle}</h1>
       <p className="w-1/3 my-2">{movieDesc}</p>
-      <div className="py-2">
-        <button className="bg-gray-400 text-white p-2 w-20 rounded opacity-80">
-          Play
+      <div className="py-2 flex justify-start items-center">
+        <button className="flex justify-center items-center bg-gray-400 text-white p-2 w-[5rem] rounded opacity-80">
+          <FaPlay className="mx-1" /> Play
         </button>
-        <button className="bg-gray-400 text-white p-2 w-28 rounded opacity-80 mx-2">
-          More Info
+        <button className="flex justify-center items-center bg-gray-400 text-white p-2 w-[8rem] rounded opacity-80 mx-2">
+          <IoMdInformationCircleOutline className="mx-1 text-2xl" /> More Info
         </button>
       </div>
     </div>
