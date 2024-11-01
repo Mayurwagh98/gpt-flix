47e0ba75fb4ee52973c2e2c2afc35f65dd8f4956
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-11-01 16:14:24 +0530

    Changed few things

--- src/components/GptSearch.jsx
+++ src/components/GptSearch.jsx
@@ -1,4 +1,5 @@
 import { LOGIN_BG_IMG } from "../utils/constants";
+import GptMoviesSuggesstions from "./GptMoviesSuggesstions";
 import GptSearchBar from "./GptSearchBar";
 
 const GptSearch = () => {
@@ -9,6 +10,7 @@ const GptSearch = () => {
         <img src={LOGIN_BG_IMG} alt="image" />
       </div>
       <GptSearchBar />
+      <GptMoviesSuggesstions />
     </>
   );
 };
